690047edf48d04f8362e05e9df2b6c049f7784be
Benjamin Calisto <bengygrand@gmail.com>
2018-10-09 22:04:43 -0300

    Updated index.html and own.js

--- assets/js/own.js
+++ assets/js/own.js
@@ -1,7 +1,9 @@
+//Adding (and importing navbar items) a left sidenav for mobile devices
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
 
-
-
-
-$(document).on('ready', () => {
-
+$(document).ready(() => {
+    $('.sidenav').sidenav();
 })
