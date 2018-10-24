7237e14f278f1e11dbb8793c8aca5868fe11b07e
Benjamin Calisto <bengygrand@gmail.com>
2018-10-24 19:02:43 -0300

    Deleted js files semicolons

--- assets/js/own.js
+++ assets/js/own.js
@@ -1,41 +1,41 @@
-var subt;
-var sm;
-var smContent;
+var subt
+var sm
+var smContent
 
-subt = document.getElementsByClassName('brand-logo-text')[0].innerHTML.substr(6,99);
-sm = document.createElement('SMALL');
-smContent = document.createTextNode(subt);
+subt = document.getElementsByClassName('brand-logo-text')[0].innerHTML.substr(6,99)
+sm = document.createElement('SMALL')
+smContent = document.createTextNode(subt)
 
 function hideBrandChild(){
-    sm.appendChild(smContent);
-    sm.setAttribute('class', 'brand-logo-text');
+    sm.appendChild(smContent)
+    sm.setAttribute('class', 'brand-logo-text')
 
     if(window.innerWidth < 601){
         //document.getElementsByClassName('brand-logo-text')[0].style.fontSize = '0';
-        document.getElementsByClassName('brand-logo')[0].innerHTML = 'TeaTeam';
+        document.getElementsByClassName('brand-logo')[0].innerHTML = 'TeaTeam'
     }else{ 
-        document.getElementsByClassName('brand-logo')[0].innerHTML = 'TeaTeam | ';
+        document.getElementsByClassName('brand-logo')[0].innerHTML = 'TeaTeam | '
         document.getElementsByClassName('brand-logo')[0].appendChild(sm);
-        document.getElementsByClassName('brand-logo-text')[0].style.fontSize = '20px';
+        document.getElementsByClassName('brand-logo-text')[0].style.fontSize = '20px'
     }
 }
 
 function dropdownPadding(){
-    var navHeight = $('header').height();
+    var navHeight = $('header').height()
     //alert($('header').height());
-    $('#modeler-items').css('padding-top', navHeight);
-    $('#studio-items').css('padding-top', navHeight);
+    $('#modeler-items').css('padding-top', navHeight)
+    $('#studio-items').css('padding-top', navHeight)
 }
 
 window.onresize = function(event){
-    hideBrandChild();
+    hideBrandChild()
 }
 
 $(function () {
-    $('.pannable-image').ImageViewer();
-});
+    $('.pannable-image').ImageViewer()
+})
 
 //dropdownPadding();
-$('.sidenav').sidenav();
-$('.dropdown-trigger').dropdown({hover: false});
-hideBrandChild();
+$('.sidenav').sidenav()
+$('.dropdown-trigger').dropdown({hover: false})
+hideBrandChild()
--- assets/js/social.js
+++ assets/js/social.js
@@ -3,18 +3,15 @@ function loadJson(){
     $.ajaxSetup({beforeSend: function(xhr){
         if (xhr.overrideMimeType)
         {
-          xhr.overrideMimeType('application/json');
+          xhr.overrideMimeType('application/json')
         }
       }
-      });
+      })
     $.getJSON('assets/json/social.json', function(json){
-        $('#social-items').html('');
+        $('#social-items').html('')
         json.forEach(e => {
-            $('#social-items').append(
-                '<a target="_blank" href="'+e.url+'" title="'+e.nam+'"><img class="social-img" src="'+e.icon+'">'
-            );
-        });
-    });
+            $('#social-items').append('<a target="_blank" href="'+e.url+'" title="'+e.nam+'"><img class="social-img" src="'+e.icon+'">')
+        })
+    })
 }
-
-loadJson();
+loadJson()
