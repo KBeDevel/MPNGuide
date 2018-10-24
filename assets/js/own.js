8daa1246c34fb7df08d9eed9adab056eb059d6e9
Benjamin Calisto <bengygrand@gmail.com>
2018-10-24 20:41:42 -0300

    Updated spacing

--- assets/js/own.js
+++ assets/js/own.js
@@ -1,15 +1,12 @@
 var subt
 var sm
 var smContent
-
 subt = document.getElementsByClassName('brand-logo-text')[0].innerHTML.substr(6,99)
 sm = document.createElement('SMALL')
 smContent = document.createTextNode(subt)
-
 function hideBrandChild(){
     sm.appendChild(smContent)
     sm.setAttribute('class', 'brand-logo-text')
-
     if(window.innerWidth < 601){
         //document.getElementsByClassName('brand-logo-text')[0].style.fontSize = '0';
         document.getElementsByClassName('brand-logo')[0].innerHTML = 'TeaTeam'
@@ -19,21 +16,17 @@ function hideBrandChild(){
         document.getElementsByClassName('brand-logo-text')[0].style.fontSize = '20px'
     }
 }
-
 function dropdownPadding(){
     var navHeight = $('header').height()
     $('#modeler-items').css('padding-top', navHeight)
     $('#studio-items').css('padding-top', navHeight)
 }
-
 window.onresize = function(event){
     hideBrandChild()
 }
-
 $(function () {
     $('.pannable-image').ImageViewer()
 })
-
 //dropdownPadding();
 $('.sidenav').sidenav()
 $('.dropdown-trigger').dropdown({hover: false})
